609805d83322c8a11f2f9b717968234d75702d39
Jonathan <j.chavez12@ufromail.cl>
2025-04-10 09:59:45 -0400

    Llenar matriz ramdon, leer matriz entera :C

--- src/Taller.java
+++ src/Taller.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Taller {
     private static Scanner sc = new Scanner(System.in);
+    private static Random rand = new Random();
 
     public static void main(String[] args) {
         // TODO: Implementar la lógica inicial y llamada a los métodos necesarios.
@@ -52,13 +54,18 @@ public class Taller {
         switch (opcion) {
             case 1 -> {
                 int[][]matriz=crearMatriz();
+                matriz=llenarMatriz(matriz);
             }
-            case 2 -> System.out.println(" 1 ");
+            case 2 -> leerFila();
             case 3 -> System.out.println(" 2 ");
             case 4 -> System.out.println("  Hasta luego...");
             default -> System.out.println(" Opcion invalida...");
         }
     }
+
+
+
+
 
     public static int[] validarDimensiones() {
         int filas = 0;
@@ -92,7 +99,34 @@ public class Taller {
         int cols = dimensiones[1];
 
         int[][] matriz = new int[filas][cols];
-
         return matriz;
+    }
+
+    private static int[][] llenarMatriz(int[][] matriz) {
+        int filas = matriz.length;
+        int cols = matriz[0].length;
+        for (int i = 0; i < filas; i++) {
+
+            for (int j = 0; j < cols; j++) {
+                matriz[i][j] = rand.nextInt(10);
+            }
+        }
+        leerMatriz(matriz);
+        return matriz;
+    }
+    public static void leerMatriz(int[][] resultado) {
+        // TODO: Implementar impresión de cada elemento de la matriz.
+        // Imprimir el resultado
+        System.out.println("\nMatriz Resultante ");
+        for (int i = 0; i < resultado.length; i++) {
+            for (int j = 0; j < resultado[i].length; j++) {
+                System.out.print(resultado[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void leerFila() {
+
     }
 }
